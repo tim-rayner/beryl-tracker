ff3ddfcc8c1716756d4f7bb6dcc98eb73ba71187
tim-rayner <tim.rayner2020@gmail.com>
2025-05-08 23:17:36 +0100

    chore: removed battery icon from vehicle icon mapper

--- src/lib/handlers/sendSummaryEmail.ts
+++ src/lib/handlers/sendSummaryEmail.ts
@@ -100,8 +100,8 @@ function formatEmail(data: NearbyVehicles): string {
 	`;
 
 	const getVehicleIcon = (type: string): string => {
-		if (type.toLowerCase().includes('scooter')) return '🔋🛴';
-		if (type.toLowerCase().includes('bbe')) return '🔋🚲';
+		if (type.toLowerCase().includes('scooter')) return '🛴';
+		if (type.toLowerCase().includes('bbe')) return '🚲';
 		if (type.toLowerCase().includes('bb')) return '🚲';
 		return '';
 	};
